d0903f66fee71f1226c75f413a80bcc07b2067b4
jatalim <jacinta.lim@insead.edu>
2017-09-26 12:50:32 +0800

    Default Profile Pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def user_avatar 
+
+if  @tweet.user.avatar.thumb.url.present?
+image_tag(@tweet.user.avatar.thumb.url)
+else
+image_tag('default.jpg', height: 50, width: 40)
+end 
 end
+
+end 
