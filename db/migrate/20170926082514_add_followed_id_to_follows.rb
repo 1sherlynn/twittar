36a03985d0b3bf457282a88e9368dee4f93088fb
Sherlynn Tan <1sherlynn@gmail.com>
2017-10-25 05:14:36 +0800

    fix follows

--- db/migrate/20170926082514_add_followed_id_to_follows.rb
+++
@@ -1,5 +0,0 @@
-class AddFollowedIdToFollows < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :follows, :followed, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926082514) do
+ActiveRecord::Schema.define(version: 20171024210846) do
 
   create_table "counters", force: :cascade do |t|
     t.string "clicks"
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(version: 20170926082514) do
   end
 
   create_table "follows", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "follower_id"
     t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
